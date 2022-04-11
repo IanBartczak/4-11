a8a7209449e8f7b12c393256d4f7bf26a26db4b3
IanBartczak <2270325@cgodin.qc.ca>
2022-04-11 19:17:32 -0400

    Initiation de mon projet Conversion

---
+++ src/Conversion.java
@@ -0,0 +1,22 @@
+public class Conversion {
+
+    public static void main(String[] args) {
+        int intNumber = 25;
+        float floatNumber = 2.6f;
+        double doubleNumber = 3.12;
+        String chaine = "vint cinq";
+
+//      intNumber = floatNumber;
+        intNumber = (int) floatNumber;
+        System.out.println(intNumber);             //shows 2, it doesn't round, it just cuts the decimal
+        intNumber = 2 + (int) floatNumber;
+
+//        chaine = (String) intNumber;               //doesn't work'
+        chaine = Integer.toString(intNumber);      //this will work.
+        System.out.println(chaine);
+
+
+
+
+    }
+}
