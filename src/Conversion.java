96ddec8dcdb9fce527ffe47581f58b95f585f66f
IanBartczak <2270325@cgodin.qc.ca>
2022-04-11 20:08:23 -0400

    i made changes to my code

--- src/Conversion.java
+++ src/Conversion.java
@@ -6,16 +6,19 @@ public class Conversion {
         double doubleNumber = 3.12;
         String chaine = "vint cinq";
 
+
 //      intNumber = floatNumber;
         intNumber = (int) floatNumber;
         System.out.println(intNumber);             //shows 2, it doesn't round, it just cuts the decimal
+
         intNumber = 2 + (int) floatNumber;
 
 //        chaine = (String) intNumber;               //doesn't work'
         chaine = Integer.toString(intNumber);      //this will work.
         System.out.println(chaine);
 
-
+//i make changes @#)($*@)#UP#JFP@)#UFP#@(UFPU#
+        //@#P(*U@FO(U@#FO(U@#
 
 
     }
